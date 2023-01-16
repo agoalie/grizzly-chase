1cfb26c17e69a77942d82038a8cadb7eb8860a3c
agoalie <104106628+agoalie@users.noreply.github.com>
2023-01-15 22:50:06 -0500

    Update Enemy.js

--- src/Enemy.js
+++ src/Enemy.js
@@ -70,26 +70,26 @@ class Enemy {
             h: player.height
         }
 
-        // var distX = Math.abs(circle.x - rect.x - rect.w / 2);
-        // var distY = Math.abs(circle.y - rect.y - rect.h / 2);
-        //
-        // if (distX > (rect.w / 2 + circle.r)) {
-        //     return false;
-        // }
-        // if (distY > (rect.h / 2 + circle.r)) {
-        //     return false;
-        // }
-        //
-        // if (distX <= (rect.w / 2)) {
-        //     return true;
-        // }
-        // if (distY <= (rect.h / 2)) {
-        //     return true;
-        // }
-        //
-        // var dx = distX - rect.w / 2;
-        // var dy = distY - rect.h / 2;
-        // return (dx * dx + dy * dy <= (circle.r * circle.r));
+        var distX = Math.abs(circle.x - rect.x - rect.w / 2);
+        var distY = Math.abs(circle.y - rect.y - rect.h / 2);
+        
+        if (distX > (rect.w / 2 + circle.r)) {
+            return false;
+        }
+        if (distY > (rect.h / 2 + circle.r)) {
+            return false;
+        }
+        
+        if (distX <= (rect.w / 2)) {
+            return true;
+        }
+        if (distY <= (rect.h / 2)) {
+            return true;
+        }
+        
+        var dx = distX - rect.w / 2;
+        var dy = distY - rect.h / 2;
+        return (dx * dx + dy * dy <= (circle.r * circle.r));
     }
 
     update() {
